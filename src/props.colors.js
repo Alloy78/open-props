5df263dc425717467d54686ccd3aaa7d83cad454
JamCow <jamcow+github@gmail.com>
2022-03-18 06:00:32 +0000

    updated colours with Toolabs

--- src/props.colors.js
+++ src/props.colors.js
@@ -1,30 +1,28 @@
-/* generated mostly with props.brand.src.js */
+/* generated mostly with Toolabs */
 export const Brand = {
+    '--black': '#000000',
+    '--white': '#ffffff',
+    '--brand-electra': '#53c6db',
+    '--brand-lilac': '#948dbc',
+    '--brand-orange': '#dd923b',
     '--brand-plum': '#564f7c',
     '--brand-teal': '#41778b',
-    '--brand-orange': '#dd923b',
-    '--brand-moave': '#948dbc',
-    '--brand-electra': '#53c6db',
     '--brand-toxic': '#83f400',
-}
-
-export const Grey = {
+    '--blue-1': '#7aa1fa',
+    '--blue-2': '#125aff',
+    '--blue-3': '#0b3ba8',
+    '--blue-4': '#02236e',
+    '--blue-5': '#001952',
+    '--green-1': '#205a00',
+    '--green-2': '#297201',
+    '--green-3': '#4e8331',
+    '--green-4': '#629d35',
+    '--green-5': '#78c350',
     '--grey-1': '#f4f0f0',
     '--grey-2': '#e6e6e6',
     '--grey-3': '#c4c4c4',
     '--grey-4': '#7c7c7c',
     '--grey-5': '#222222',
-}
-
-export const Green = {
-    '--green-1': '#78c350',
-    '--green-2': '#629d35',
-    '--green-3': '#4e8331',
-    '--green-4': '#297201',
-    '--green-5': '#205a00',
-}
-
-export const Red = {
     '--red-1': '#fc7c7c',
     '--red-2': '#ff5d5d',
     '--red-3': '#e22a2a',
@@ -32,22 +30,8 @@ export const Red = {
     '--red-5': '#980000',
 }
 
-export const Blue = {
-    '--blue-1': '#7aa1fa',
-    '--blue-2': '#125aff',
-    '--blue-3': '#0b3ba8',
-    '--blue-4': '#02236e',
-    '--blue-5': '#001952',
-}
-
-/* generated mostly with props.colors.src.js */
-
 const Colors = {
     ...Brand,
-    ...Grey,
-    ...Green,
-    ...Red,
-    ...Blue
 }
 
 export default Colors
